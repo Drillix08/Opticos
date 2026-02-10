069deadb54ea352c0e6aeb80cb8d6e56c803dd5e
Marcus Moreno <triforcehero03@gmail.com>
2026-02-10 18:08:40 -0500

    Small optimization

--- source/graph-environment/grid.gd
+++ source/graph-environment/grid.gd
@@ -29,19 +29,19 @@ func refresh_pixel_positions(xOffset, yOffset):
 
 func _draw():
 	var windowSize :Vector2i = DisplayServer.window_get_size()
-	
+	#offset values the pixel positions of the graph objects 
+	#have from their original positions when the origin was at initialization
+	var refreshedPosition :Array = refresh_pixel_positions(origin.x, origin.y)
+	var xOffset :float = refreshedPosition[0]
+	var yOffset :float = refreshedPosition[1]
+		
 	#Draw the vertical lines of the grid
 	for x in range(0, 2*(int(windowSize.x)), grid_spacing):
 		var color := Color(0.3, 0.3, 0.3)
 		@warning_ignore("integer_division")
 		if (x / grid_spacing) % major_every == 0:
 			color = Color(0.5, 0.5, 0.5)
-		#offset values the pixel positions of the graph objects 
-		#have from their original positions when the origin was at initialization
-		var refreshedPosition :Array = refresh_pixel_positions(origin.x, origin.y)
-		var xOffset :float = refreshedPosition[0]
-		var yOffset :float = refreshedPosition[1]
-		#Draw y-axis
+		#Draw solid line
 		draw_line(Vector2(x+xOffset, yOffset-5*grid_spacing), Vector2(x+xOffset, 2*windowSize.y+yOffset-5*grid_spacing), color)
 
 	#Draw the horizontal lines of the grid
@@ -50,17 +50,12 @@ func _draw():
 		@warning_ignore("integer_division")
 		if (y / grid_spacing) % major_every == 0:
 			color = Color(0.5, 0.5, 0.5)
-		#offset values the pixel positions of the graph objects 
-		#have from their original positions when the origin was at initialization
-		var refreshedPosition :Array = refresh_pixel_positions(origin.x, origin.y)
-		var xOffset :float = refreshedPosition[0]
-		var yOffset :float = refreshedPosition[1]
-		#Draw x-axis
+		#Draw solid line
 		draw_line(Vector2(xOffset-5*grid_spacing, y+yOffset), Vector2(2*windowSize.x+xOffset-5*grid_spacing, y+yOffset), color)
 	
 	#Draw the x and y axes
 	draw_line(Vector2(500, -origin.y) + origin, Vector2(500, 700-origin.y) + origin, Color(0.0, 0.0, 0.0, 1.0), 5.0)
-	draw_line(Vector2(-origin.x, 300) + origin, Vector2(1000-origin.x, 300) + origin, Color(0.0, 0.0, 0.0, 1.0), 5.0)
+	draw_line(Vector2(0, 300 + origin.y), Vector2(1000-origin.x, 300) + origin, Color(0.0, 0.0, 0.0, 1.0), 5.0)
 
 #controlls the moving of the "camera" when you click and drag
 func _input(event):
