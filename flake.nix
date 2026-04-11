91386e126366a7fc24c5c1587797e27495d7c0cb
Cass1312 <github.concept275@passmail.net>
2026-04-11 19:03:12 -0400

    Added flake.nix for help with dev environment

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        pkg-config
+        ginac
+        cln
+        libgcc
+        scons
+      ];
+
+      shellHook = ''
+        bash
+      '';
+    };
+  };
+}
