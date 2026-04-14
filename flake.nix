aa932be1808e193852a267fffbe36d933b2a019f
Cass1312 <github.concept275@passmail.net>
2026-04-14 17:51:51 -0400

    setting up gitignore for compilation

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     system = "x86_64-linux";
   in {
     devShells."${system}".default = let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = (import nixpkgs { inherit system; });
     in pkgs.mkShell {
       packages = with pkgs; [
         pkg-config
